aec47529e8fabbf1684f69ecceef2811fbff5f13
zack mccann <zackjmccann@gmail.com>
2019-09-13 10:55:49 -0600

    PyBank Edit3

--- PyBank/main.py
+++ PyBank/main.py
@@ -6,13 +6,10 @@ import csv
 #Path to the budget data file from PyBank directory
 pybank_csv = os.path.join("../", "../", "Homework Instructions", "PyBank", "Resources", "budget_data.csv")
 
-#Lists for the months and profit/losses
+#Variables
 months = []
 profit_losses = []
-
-num_of_months = 0
 total_pl = 0
-pro_loss_total = 0
 
 #Read into the budget data file
 with open(pybank_csv, 'r') as csvfile:
@@ -25,16 +22,12 @@ with open(pybank_csv, 'r') as csvfile:
     for num in profit_losses:
         total_pl += num
     
-    num_of_months = str(len(months))
-    pro_loss_total = str(total_pl)
-
-    def totalMonths(allMonths):
-        print("Total Months: " + allMonths)
-    def sumOfProLoss(proLossSum):
-        print("Profit/Loss Total: " + pro_loss_total)
+    num_of_months = (str(len(months)))
+    pro_loss_total = (str(total_pl))
 
 #Zip the new data together
-#cleaned_file = zip(months, profit_losses)
+# cleaned_file = zip(num_of_months, pro_loss_total)
+cleaned_file = zip([num_of_months], [pro_loss_total])
 
 #Create a new csv document for the results
 analyzed_data = os.path.join("pybank_analyzed.csv")
@@ -42,6 +35,6 @@ analyzed_data = os.path.join("pybank_analyzed.csv")
 #Write the new file
 with open(analyzed_data, 'w') as data:
     writer = csv.writer(data)
-    writer.writerow([totalMonths(num_of_months)])
-    writer.writerow([sumOfProLoss(pro_loss_total)])
-    #writer.writerows(cleaned_file)
+    # writer.writerow(["months",'pro_loss_total'])
+    # writer.writerow([pro_loss_total])
+    writer.writerows(cleaned_file)
