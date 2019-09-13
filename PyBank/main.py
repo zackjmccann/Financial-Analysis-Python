41176c7ca4d03cf5567d7867d1bc74bdfef7ff21
zack mccann <zackjmccann@gmail.com>
2019-09-13 10:15:57 -0600

    PyBank Edits2

--- PyBank/main.py
+++ PyBank/main.py
@@ -10,13 +10,28 @@ pybank_csv = os.path.join("../", "../", "Homework Instructions", "PyBank", "Reso
 months = []
 profit_losses = []
 
+num_of_months = 0
+total_pl = 0
+pro_loss_total = 0
+
 #Read into the budget data file
 with open(pybank_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader, None)
     for row in csvreader:
         months.append(row[0])
-        #profit_losses.append(row[1])
+        profit_losses.append(int(row[1]))
+
+    for num in profit_losses:
+        total_pl += num
+    
+    num_of_months = str(len(months))
+    pro_loss_total = str(total_pl)
+
+    def totalMonths(allMonths):
+        print("Total Months: " + allMonths)
+    def sumOfProLoss(proLossSum):
+        print("Profit/Loss Total: " + pro_loss_total)
 
 #Zip the new data together
 #cleaned_file = zip(months, profit_losses)
@@ -27,5 +42,6 @@ analyzed_data = os.path.join("pybank_analyzed.csv")
 #Write the new file
 with open(analyzed_data, 'w') as data:
     writer = csv.writer(data)
-    writer.writerow([len(months)])
+    writer.writerow([totalMonths(num_of_months)])
+    writer.writerow([sumOfProLoss(pro_loss_total)])
     #writer.writerows(cleaned_file)
