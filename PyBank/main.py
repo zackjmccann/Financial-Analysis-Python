fb90fd51f039a803c65f81a5a364f5dc84a63fb7
zack mccann <zackjmccann@gmail.com>
2019-09-19 09:21:50 -0600

    Edit 5

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,22 +8,72 @@ pybank_csv = os.path.join("../", "../", "Homework Instructions", "PyBank", "Reso
 
 #Variables
 months = []
-profit_losses = []
+profit_loss = []
+change_list = []
 total_pl = 0
+sum_change = 0
+
+#Functions
+def average_change(pro_loss):
+    current_month = 1
+    prev_month = 0
+    for x in pro_loss:
+        if current_month <= 85:
+            x = pro_loss[current_month] - pro_loss[prev_month]
+            change_list.append(x)
+            current_month += 1
+            prev_month += 1
+
+def increase(changes):
+    greatest_increase = 0
+    value = 0
+    for x in changes:
+        while value <= 84:
+            if changes[value] > greatest_increase:
+                greatest_increase = changes[value]
+            value += 1
+    return greatest_increase
+
+def decrease(changes):
+    greatest_decrease = 0
+    value = 0
+    for x in changes:
+        while value <= 84:
+            if changes[value] < greatest_decrease:
+                greatest_decrease = changes[value]
+            value += 1
+    return greatest_decrease
+
 
 #Read into the budget data file
 with open(pybank_csv, 'r', newline="") as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader, None)
+
+    #Make a list of all the Months and the profit/losses
     for row in csvreader:
         months.append(row[0])
-        profit_losses.append(int(row[1]))
-
-    for num in profit_losses:
-        total_pl += num
+        profit_loss.append(int(row[1]))
     
+    #Find the total profit/loss
+    for num in profit_loss:
+        total_pl += num
+
+    #Calculate the number of months
     num_of_months = (str(len(months)))
+
+    #Calculate the total profit loss
     pro_loss_total = (str(total_pl))
+
+    #Calculate the average change
+    average_change(profit_loss)
+    
+    for change in change_list:
+        sum_change += change
+
+    avg_change = (str(round((sum_change/85),2)))
+    grt_increase = (str(increase(change_list)))
+    grt_decrease = (str(decrease(change_list)))
 
 #Create a new csv document for the results
 analyzed_data = os.path.join("pybank_analyzed.csv")
@@ -33,4 +83,7 @@ with open(analyzed_data, "w", newline="") as data:
     writer = csv.writer(data, delimiter=',')
     writer.writerow(["Financial Analysis"])
     writer.writerow(["Total Months: " + num_of_months])
-    writer.writerow(["Toatl Profit/Loss: " + pro_loss_total])
+    writer.writerow(["Total Profit/Loss: $" + pro_loss_total])
+    writer.writerow(["Average Profit/Loss Change: $" + avg_change])
+    writer.writerow(["Greatest Increase in Profits: $" + grt_increase])
+    writer.writerow(["Greatest Decrease in Profits: $" + grt_decrease])
