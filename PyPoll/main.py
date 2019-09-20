d9a632bbba00ed38d061c5933838b937d2afb7bf
zack mccann <zackjmccann@gmail.com>
2019-09-19 18:46:34 -0600

    Edit 1

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -2,21 +2,20 @@
 
 import os
 import csv
+from collections import Counter
 
 #Path to the election data file from PyPoll directory
 pypoll_csv = os.path.join("../", "../", "Homework Instructions", "PyPoll", "Resources", "election_data.csv")
 
 voter_id = []
 county = []
+voter_cand = []
+
 candidate = []
+cand_votes = []
 
 #Functions
-def list_all_candidates(all_candidates):
-    candidate_list = []
-    for x in all_candidates: 
-        if x not in candidate_list: 
-            all_candidates.append(x)
-    return candidate_list
+
 
 #Read into the election data file
 with open(pypoll_csv, 'r', newline="") as csvfile:
@@ -26,14 +25,26 @@ with open(pypoll_csv, 'r', newline="") as csvfile:
     #Place all data in list
     for row in csvreader:
             voter_id.append(row[0])
-            county.append(row[1])
-            candidate.append(row[2])
+            # county.append(row[1])
+            voter_cand.append(row[2])
 
     #Calculate the number of votes
     num_of_votes = (str(len(voter_id)))
 
     #List of candidates who received votes
-    all_cans = list_all_candidates(candidate)
+    for x in Counter(voter_cand).keys():
+        candidate.append(x)
+    
+    for x in Counter(voter_cand).values():
+        cand_votes.append(x)
+
+    can_one = (str(candidate[0])) + " " +(str(cand_votes[0]))
+    can_two = (str(candidate[1])) + " " +(str(cand_votes[1]))
+    can_three = (str(candidate[2])) + " " +(str(cand_votes[2]))
+    can_four = (str(candidate[3])) + " " +(str(cand_votes[3]))
+
+    #Calculate the Winner
+    
 
 #Create a new csv document for the results
 analyzed_data = os.path.join("pypoll_analyzed.csv")
@@ -43,4 +54,7 @@ with open(analyzed_data, "w", newline="") as data:
     writer = csv.writer(data, delimiter=',')
     writer.writerow(["Election Analysis"])
     writer.writerow(["Total Votes: " + num_of_votes])
-    writer.writerow(["Test Row: " + all_cans])
+    writer.writerow([can_one])
+    writer.writerow([can_two])
+    writer.writerow([can_three])
+    writer.writerow([can_four])
